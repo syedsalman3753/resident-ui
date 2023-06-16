e39c50e58405e010e685fe1d4767f3787ba40a71
Madhuravas <madhuravasreddy1998@gmail.com>
2023-06-16 09:50:34 +0530

    resolved default language name issue

--- resident-ui/src/app/feature/uinservices/sharewithpartner/sharewithpartner.component.ts
+++ resident-ui/src/app/feature/uinservices/sharewithpartner/sharewithpartner.component.ts
@@ -230,10 +230,18 @@ export class SharewithpartnerComponent implements OnInit, OnDestroy {
                 this.fullAddress = this.fullAddress.replace(/^./, "");
                 value = this.fullAddress
               } else {
-                value = this.userInfo[data.attributeName][0].value;
+                this.userInfo[data.attributeName].forEach(item =>{
+                  if(item.language === this.langCode){
+                    value = item.value
+                  }
+                });
               }
             } else {
-              value = this.userInfo[data.attributeName][0].value;
+              this.userInfo[data.attributeName].forEach(item =>{
+                if(item.language === this.langCode){
+                  value = item.value
+                }
+              });
             }
         }
 
