67874dc1a9878cfcf771b153bdd5a1471f7be576
Madhuravas <madhuravasreddy1998@gmail.com>
2023-08-08 11:49:59 +0530

    MOSIP-28824 added link for registration centers

--- resident-ui/src/app/shared/dialog/dialog.component.ts
+++ resident-ui/src/app/shared/dialog/dialog.component.ts
@@ -210,6 +210,11 @@ export class DialogComponent implements OnInit {
     this.dialog.closeAll();
   }
 
+  regCenters(){
+    this.router.navigate(['regcenter'])
+    this.dialog.closeAll();
+  }
+
   sendResponse(value: any) {
     if (value.length > 0) {
       this.submitBtnDisabled = true
