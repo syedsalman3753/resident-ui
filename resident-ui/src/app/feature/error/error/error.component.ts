f67ae24901952149a86a741bdbad20a6b1a1628c
Madhuravas <madhuravasreddy1998@gmail.com>
2023-08-10 11:27:35 +0530

    MOSIP-28542 redirecting to login page

--- resident-ui/src/app/feature/error/error/error.component.ts
+++ resident-ui/src/app/feature/error/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginRedirectService } from 'src/app/core/services/loginredirect.service';
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private redirectService: LoginRedirectService
+  ) { }
 
   ngOnInit() {
   }
-
+  login(){
+    this.redirectService.redirect(window.location.href);
+  }
 }
