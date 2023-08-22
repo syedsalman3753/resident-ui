6f4324b1109ac8a430b82de5b36754b3eb18fe5f
Aravindhan Alagesan <aravindh2090@gmail.com>
2023-08-22 17:16:28 +0530

    Update loginredirect.service.ts
    Signed-off-by: Aravindhan Alagesan <aravindh2090@gmail.com>

--- resident-ui/src/app/core/services/loginredirect.service.ts
+++ resident-ui/src/app/core/services/loginredirect.service.ts
@@ -14,7 +14,7 @@ export class LoginRedirectService {
     if(url.split("#")[1] === "/dashboard"){
       constructurl = url.replace("/dashboard", "/uinservices/dashboard");
     }
-    window.location.href = `${this.appService.getConfig().baseUrl}${this.appService.getConfig().login}` + btoa(constructurl)+"?state="+stateParam+"&ui_locales="+defaultJson["keyboardMapping"][localStorage.getItem("langCode")];
+    window.location.href = `${this.appService.getConfig().baseUrl}${this.appService.getConfig().login}v2/` + btoa(constructurl)+"?state="+stateParam+"&ui_locales="+defaultJson["keyboardMapping"][localStorage.getItem("langCode")];
     localStorage.setItem("redirectURL", constructurl)
   }
 
