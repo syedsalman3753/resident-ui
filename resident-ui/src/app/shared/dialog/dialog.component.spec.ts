7c1d829ab044de92656ef83ec680d497d2cf9ad2
Aravindhan Alagesan <aravindh2090@gmail.com>
2023-09-07 16:30:36 +0530

    Changes related to unit testing

--- resident-ui/src/app/shared/dialog/dialog.component.spec.ts
+++ resident-ui/src/app/shared/dialog/dialog.component.spec.ts
@@ -1,25 +1,75 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 import { DialogComponent } from './dialog.component';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let mockMatDialogRef: MatDialogRef<DialogComponent>;
+  let mockMatDialog: MatDialog;
+  let mockRouter: Router;
+  let mockTranslateService: TranslateService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
-    })
-    .compileComponents();
-  }));
+  const mockDialogData = {
+    title: 'Test',
+    // Add any other properties you need for testing here
+  };
 
   beforeEach(() => {
+    mockMatDialogRef = jasmine.createSpyObj(['close']);
+    mockMatDialog = jasmine.createSpyObj(['closeAll']);
+    mockRouter = jasmine.createSpyObj(['navigateByUrl']);
+
+    TestBed.configureTestingModule({
+      declarations: [DialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: mockMatDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData },
+        { provide: MatDialog, useValue: mockMatDialog },
+        { provide: Router, useValue: mockRouter },
+        { provide: TranslateService, useValue: mockTranslateService },
+      ],
+    });
+
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the DialogComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close the dialog when calling onNoClick', () => {
+    component.onNoClick();
+    expect(mockMatDialogRef.close).toHaveBeenCalled();
+  });
+
+  it('should navigate to the specified URL when calling viewDetails', () => {
+    const eventId = '12345';
+    component.viewDetails(eventId);
+    expect(mockRouter.navigateByUrl).toHaveBeenCalledWith(`uinservices/trackservicerequest?eid=${eventId}`);
+  });
+
+  it('should toggle isChecked property when calling agreeConditions', () => {
+    const initialCheckedState = component.isChecked;
+    component.agreeConditions();
+    expect(component.isChecked).toBe(!initialCheckedState);
+  });
+
+  it('should set otpTimeMinutes and disableInput properties when calling setOtpTime', () => {
+    component.appConfigService = {
+      getConfig: () => ({ 'mosip.kernel.otp.expiry-time': 120 }),
+    } as any;
+
+    component.setOtpTime();
+
+    expect(component.otpTimeMinutes).toBe(2);
+    expect(component.disableInput).toBe(false);
+  });
+
+  // Add more test cases as needed for other methods and properties
+
 });
