0ddcb57b954cf8477df2133af3d2e666cc620d7b
Madhuravas <madhuravasreddy1998@gmail.com>
2023-11-27 17:40:48 +0530

    added default values
    Signed-off-by: Madhuravas <madhuravasreddy1998@gmail.com>

--- resident-ui/src/app/feature/booking/map/map.component.ts
+++ resident-ui/src/app/feature/booking/map/map.component.ts
@@ -32,9 +32,9 @@ export class MapComponent implements OnInit {
   url: string;
   centers: any;
   markers = [];
-  zoom:number;
-  minZoom:number;
-  maxZoom:number;
+  zoom:number = 14;
+  minZoom:number = 5;
+  maxZoom:number = 18;
 
   googleMapsUrl =
     'https://maps.google.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i375060738!3m9!2spl!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0';
