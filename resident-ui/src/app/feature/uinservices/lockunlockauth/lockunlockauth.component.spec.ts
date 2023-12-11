423d07a4cc9dab89d2197bda763effffb63f9203
Aravindhan Alagesan <aravindh2090@gmail.com>
2023-12-11 11:49:59 +0530

    Sonar coverage related code changes

--- resident-ui/src/app/feature/uinservices/lockunlockauth/lockunlockauth.component.spec.ts
+++ resident-ui/src/app/feature/uinservices/lockunlockauth/lockunlockauth.component.spec.ts
@@ -90,7 +90,6 @@ describe('LockunlockauthComponent', () => {
 
     translateServiceSpy.getTranslation.and.returnValue(of(mockResponse));
     appConfigServiceSpy.getConfig.and.returnValue(mockAppConfig);
-    autoLogoutServiceSpy.currentMessageAutoLogout.and.returnValue(of({ timerFired: false }));
     autoLogoutServiceSpy.getValues.and.returnValue(null);
 
     component.ngOnInit();
