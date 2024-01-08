ef52dcdf089d317ed8c30fcd134f6dc4d0a387af
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2024-01-08 11:33:48 +0530

    MOSIP-30902
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- residenttest/src/main/java/io/mosip/testrig/residentui/testcase/GetMyUIN.java
+++ residenttest/src/main/java/io/mosip/testrig/residentui/testcase/GetMyUIN.java
@@ -11,29 +11,26 @@ import io.mosip.testrig.residentui.utility.Commons;
 import io.mosip.testrig.residentui.utility.JsonUtil;
 import io.mosip.testrig.residentui.utility.MockSMTPListener;
 import io.mosip.testrig.residentui.utility.ResidentBaseClass;
+import io.mosip.testrig.residentui.utility.TestRunner;
 
 @Test(groups = "GMU")
 public class GetMyUIN extends ResidentBaseClass{
-
+	
+	String vid =TestRunner.perpetualVid;
 	// @Test(groups = "GMU")
 	public void getMyUIN() throws Exception {
+		
 		String tempemail = JsonUtil.JsonObjParsing(Commons.getTestData(), "tempemail");
-		String aid = JsonUtil.JsonObjParsing(Commons.getTestData(), "aid");
-
 		test = extent.createTest("getMyUIN", "verify Login");
-		Commons.click(test, driver, By.xpath("(//*[@id='dashboardCard4'])[1]"));
-		Commons.enter(test, driver, By.id("aidValue"), aid);//
+		Commons.click(test, driver, By.id("getMyUIN"));
+		Commons.enter(test, driver, By.id("aidValue"), vid);//
 		test.log(Status.INFO, "AID Entered");
-		Commons.switchToFrameByIndex(test, driver, 0);
-		Commons.click(test, driver, By.xpath("//div[@id='rc-anchor-container']"));
-		Thread.sleep(3000);
-		driver.switchTo().parentFrame();
 		Commons.click(test, driver, By.id("getUinSendOtpBtn"));
 		String otp = MockSMTPListener.getOtp(tempemail);
 		Commons.enter(test, driver, By.id("otp-input"), otp);
 		Commons.click(test, driver, By.xpath("//button[@id='getUinsubmitBtn']"));
 		test.log(Status.INFO, "Click on submit");
-		Commons.click(test, driver, By.id("confirmmessagepopup"));
+		Commons.click(test, driver, By.id("dismissBtn"));
 		
 		
 		
@@ -41,64 +38,62 @@ public class GetMyUIN extends ResidentBaseClass{
 	
 	public void getMyUINWithoutAID() throws InterruptedException, IOException {
 		test=extent.createTest("get MyUIN Without AID", "verify Login");
-		Commons.click(test, driver, By.xpath("(//*[@id='dashboardCard4'])[1]"));
-		Commons.switchToFrameByIndex(test,driver, 0);
-		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
-		Thread.sleep(3000);
-		driver.switchTo().parentFrame();
+		Commons.click(test, driver, By.id("getMyUIN"));
+//		Commons.switchToFrameByIndex(test,driver, 0);
+//		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
+//		Thread.sleep(3000);
+//		driver.switchTo().parentFrame();
 		Commons.click(test,driver, By.id("getUinSendOtpBtn"));
 	}
 	
 	
 	public void getMyUINWithInvalidOtp() throws Exception {
 		String tempemail=JsonUtil.JsonObjParsing(Commons.getTestData(),"tempemail");
-		String aid=JsonUtil.JsonObjParsing(Commons.getTestData(),"aid");
+		
 		test=extent.createTest("get My UIN With Invalid Otp", "verify Login");
-		Commons.click(test, driver, By.xpath("(//*[@id='dashboardCard4'])[1]"));
-		Commons.enter(test,driver, By.id("aidValue"), aid);//
-		Commons.switchToFrameByIndex(test,driver, 0);
-		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
-		Thread.sleep(3000);
-		driver.switchTo().parentFrame();
+		Commons.click(test, driver, By.id("getMyUIN"));
+		Commons.enter(test,driver, By.id("aidValue"), vid);//
+//		Commons.switchToFrameByIndex(test,driver, 0);
+//		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
+//		Thread.sleep(3000);
+//		driver.switchTo().parentFrame();
 		Commons.click(test,driver, By.id("getUinSendOtpBtn"));
 		String otp = MockSMTPListener.getOtp(tempemail);
 		
 		Commons.enter(test,driver, By.id("otp-input"), otp+"56");
 		   Commons.click(test,driver, By.xpath("//button[@id='getUinsubmitBtn']"));
-		   Commons.click(test,driver, By.id("confirmmessagepopup"));
+		   Commons.click(test,driver, By.id("dismissBtn"));
 		
 	}
 	
 	public void getMyUINWithExpiredOtp() throws Exception {
 		String tempemail=JsonUtil.JsonObjParsing(Commons.getTestData(),"tempemail");
-		String aid=JsonUtil.JsonObjParsing(Commons.getTestData(),"aid");
+		
 		test=extent.createTest("get My UIN With Expired Otp", "verify Login");
-		Commons.click(test, driver, By.xpath("(//*[@id='dashboardCard4'])[1]"));//id
-		Commons.enter(test,driver, By.id("aidValue"), aid);//
-		Commons.switchToFrameByIndex(test,driver, 0);
-		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
-		Thread.sleep(3000);
-		driver.switchTo().parentFrame();
+		Commons.click(test, driver, By.id("getMyUIN"));//id
+		Commons.enter(test,driver, By.id("aidValue"), vid);//
+	//	Commons.switchToFrameByIndex(test,driver, 0);
+//		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
+//		Thread.sleep(3000);
+//		driver.switchTo().parentFrame();
 		Commons.click(test,driver, By.id("getUinSendOtpBtn"));
 		String otp = MockSMTPListener.getOtp(tempemail);
 		Thread.sleep(180020);
 		Commons.enter(test,driver, By.id("otp-input"), otp);
 		   Commons.click(test,driver, By.xpath("//button[@id='getUinsubmitBtn']"));
-		   Commons.click(test,driver, By.id("confirmmessagepopup"));
+		   Commons.click(test,driver, By.id("dismissBtn"));
 		   
 	}
 	
 	
 	public void getMyUINWithoutOtp() throws Exception {
 		test=extent.createTest("get My UIN Without Otp", "verify Login");
-		String tempemail=JsonUtil.JsonObjParsing(Commons.getTestData(),"tempemail");
-		String aid=JsonUtil.JsonObjParsing(Commons.getTestData(),"aid");
-		Commons.click(test, driver, By.xpath("(//*[@id='dashboardCard4'])[1]"));//id
-		Commons.enter(test,driver, By.id("aidValue"), aid);//
-		Commons.switchToFrameByIndex(test,driver, 0);
-		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
-		Thread.sleep(3000);
-		driver.switchTo().parentFrame();
+		Commons.click(test, driver, By.id("getMyUIN"));//id
+		Commons.enter(test,driver, By.id("aidValue"), vid);//
+//		Commons.switchToFrameByIndex(test,driver, 0);
+//		Commons.click(test,driver, By.xpath("//div[@id='rc-anchor-container']"));
+//		Thread.sleep(3000);
+//		driver.switchTo().parentFrame();
 		Commons.click(test,driver, By.id("getUinSendOtpBtn"));
 		   Commons.click(test,driver, By.xpath("//button[@id='getUinsubmitBtn']"));
 		   
--- residenttest/src/main/java/io/mosip/testrig/residentui/testcase/ShareMyData.java
+++ residenttest/src/main/java/io/mosip/testrig/residentui/testcase/ShareMyData.java
@@ -17,50 +17,50 @@ public class ShareMyData extends BaseClass {
 		test = extent.createTest("shareMyData Test", "verify shareMyData");
 		Commons.clickWebelement(test, driver, By.id("uinservices/sharewithpartner"));
 
-		Commons.clickWebelement(test, driver, By.id("Name"));
-		Commons.clickWebelement(test, driver, By.id("DOB"));
+		Commons.clickWebelement(test, driver, By.id("name"));
+		Commons.clickWebelement(test, driver, By.id("dateOfBirth"));
 		Commons.clickWebelement(test, driver, By.id("UIN"));
 		Commons.dropdown(test, driver, By.id("partnerDetails"));
 		test.log(Status.INFO, "Select Dropdown PartnerDetails");
 		Commons.enter(test, driver, By.id("sharingReasonPlaceholder"), data);
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
-		Commons.clickWebelement(test, driver, By.className("conditions-checkbox"));
-		Commons.clickWebelement(test, driver, By.xpath("(//span[text()='Share'])[2]"));
+		Commons.clickWebelement(test, driver, By.id("confirmShareInfo"));
+		Commons.clickWebelement(test, driver, By.id("shareInfoBtn"));
 		test.log(Status.INFO, "Click On Share");
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// without partner name and purpose
 		Thread.sleep(3000);
 		test.log(Status.INFO, "Click On Share without partner name and purpose");
 		Commons.clickWebelement(test, driver, By.id("uinservices/sharewithpartner"));
-		Commons.clickWebelement(test, driver, By.id("Name"));
-		Commons.clickWebelement(test, driver, By.id("DOB"));
+		Commons.clickWebelement(test, driver, By.id("name"));
+		Commons.clickWebelement(test, driver, By.id("dateOfBirth"));
 		Commons.clickWebelement(test, driver, By.id("UIN"));
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
 		test.log(Status.INFO, "Click On Share");
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// without partner name
 		Thread.sleep(3000);
 		test.log(Status.INFO, "Click On Share without partner name");
 		Commons.enter(test, driver, By.id("sharingReasonPlaceholder"), data);
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// without purpose
 		test.log(Status.INFO, "Click On without purpose");
 		driver.findElement(By.id("sharingReasonPlaceholder")).clear();
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// all with valid
 		test.log(Status.INFO, "all with valid");
-		Commons.clickWebelement(test, driver, By.id("Perpetual VID"));
-		Commons.clickWebelement(test, driver, By.id("Phone Number"));
-		Commons.clickWebelement(test, driver, By.id("Email ID"));
-		Commons.clickWebelement(test, driver, By.id("Address"));
-		Commons.clickWebelement(test, driver, By.id("Gender"));
-		Commons.clickWebelement(test, driver, By.id("Image"));
+		Commons.clickWebelement(test, driver, By.id("perpetualVID"));
+		Commons.clickWebelement(test, driver, By.id("phone"));
+		Commons.clickWebelement(test, driver, By.id("email"));
+		Commons.clickWebelement(test, driver, By.id("fullAddress"));
+		Commons.clickWebelement(test, driver, By.id("gender"));
+		Commons.clickWebelement(test, driver, By.id("photo"));
 //		Commons.clickWebelement(test, driver, By.id("Mask It2"));
 	//	Commons.clickWebelement(test, driver, By.id("Mask It3"));
 		//Commons.clickWebelement(test, driver, By.id("Mask It4"));
@@ -74,33 +74,33 @@ public class ShareMyData extends BaseClass {
 		test.log(Status.INFO, "Select Dropdown");
 		Commons.enter(test, driver, By.id("sharingReasonPlaceholder"), data);
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
-		Commons.clickWebelement(test, driver, By.className("conditions-checkbox"));
-		Commons.clickWebelement(test, driver, By.xpath("(//span[text()='Share'])[2]"));
-		test.log(Status.INFO, "Click Share");
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("confirmShareInfo"));
+		Commons.clickWebelement(test, driver, By.id("shareInfoBtn"));
+		test.log(Status.INFO, "Click On Share");
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// without purpose and partner
 		test.log(Status.INFO, "Share My Data without purpose and partner");
 		Commons.clickWebelement(test, driver, By.id("uinservices/sharewithpartner"));
-		Commons.clickWebelement(test, driver, By.id("Name"));
-		Commons.clickWebelement(test, driver, By.id("DOB"));
+		Commons.clickWebelement(test, driver, By.id("name"));
+		Commons.clickWebelement(test, driver, By.id("dateOfBirth"));
 		Commons.clickWebelement(test, driver, By.id("UIN"));
-		Commons.clickWebelement(test, driver, By.id("Perpetual VID"));
-		Commons.clickWebelement(test, driver, By.id("Phone Number"));
-		Commons.clickWebelement(test, driver, By.id("Email ID"));
-		Commons.clickWebelement(test, driver, By.id("Address"));
-		Commons.clickWebelement(test, driver, By.id("Gender"));
-		Commons.clickWebelement(test, driver, By.id("Image"));
+		Commons.clickWebelement(test, driver, By.id("perpetualVID"));
+		Commons.clickWebelement(test, driver, By.id("phone"));
+		Commons.clickWebelement(test, driver, By.id("email"));
+		Commons.clickWebelement(test, driver, By.id("fullAddress"));
+		Commons.clickWebelement(test, driver, By.id("gender"));
+		Commons.clickWebelement(test, driver, By.id("photo"));
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
 		test.log(Status.INFO, "Click Share");
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 		// without partnername
 		test.log(Status.INFO, "Share My Data without partnername");
 		Commons.enter(test, driver, By.id("sharingReasonPlaceholder"), data);
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
 		test.log(Status.INFO, "Click Share");
-		Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 		driver.findElement(By.id("sharingReasonPlaceholder")).clear();
 
 		// without purpose
@@ -108,7 +108,7 @@ public class ShareMyData extends BaseClass {
 		Commons.dropdown(test, driver, By.id("partnerDetails"));
 		Commons.clickWebelement(test, driver, By.id("shareBtn"));
 		test.log(Status.INFO, "Click Share");
-		//Commons.clickWebelement(test, driver, By.id("confirmmessagepopup"));
+		Commons.clickWebelement(test, driver, By.id("dismissBtn"));
 
 	}
 }
